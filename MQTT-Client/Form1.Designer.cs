75010bcf16659d64511525573385f468dc5538e2
eheeeun <38809474+eheeeun@users.noreply.github.com>
2020-03-11 02:08:33 +0900

    Update Form1.Designer.cs

--- MQTT-Client/Form1.Designer.cs
+++ MQTT-Client/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// 設計工具所需的變數。
+        /// 
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清除任何使用中的資源。
+        /// 
         /// </summary>
         /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
         protected override void Dispose(bool disposing)
