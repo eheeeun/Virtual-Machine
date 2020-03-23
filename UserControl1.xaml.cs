bf118dede61f0c6de1d41370302aac3af7798bae
eheeeun <38809474+eheeeun@users.noreply.github.com>
2020-03-23 14:43:46 +0900

    add mid value and method from original
    about mid temperature

--- UserControl1.xaml.cs
+++ UserControl1.xaml.cs
@@ -15,13 +15,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Thermometer
+namespace Thermometer /* mid온도 값 위한 메서드 및 변수 추가 */
 {
     /// <summary>
     /// UserControl1.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class UserControl1 : UserControl, INotifyPropertyChanged
     {
+        /* 속성 변경알림 구현 https://docs.microsoft.com/ko-kr/dotnet/framework/wpf/data/how-to-implement-property-change-notification */
         public event PropertyChangedEventHandler PropertyChanged;
         public void NotifyPropertyChanged([CallerMemberName]string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
@@ -31,7 +32,7 @@ namespace Thermometer
             get { return isCelsius; }
             set
             {
-                isCelsius = value;
+                isCelsius = value; // value는 새로 들어오는 값을 의미 public bool isCelsius와 같은 의미라는데 잘 모르겠다..
                 NotifyPropertyChanged(nameof(MinTemperatureStr));
                 NotifyPropertyChanged(nameof(MaxTemperatureStr));
                 NotifyPropertyChanged(nameof(TemperatureText));
@@ -49,21 +50,22 @@ namespace Thermometer
                 NotifyPropertyChanged(nameof(TemperatureHeight));
                 NotifyPropertyChanged(nameof(MinTemperatureStr));
             }
-        }   
-        public static readonly DependencyProperty TemperatureProperty =
-            DependencyProperty.Register(
-                "Temperature", 
-                typeof(double), 
-                typeof(UserControl1), 
-                new PropertyMetadata(37.0)
-            );
-
-        public string MinTemperatureStr
-        {
-            get { return $"{(int)minTemp}°" + (isCelsius ? "C" : "F"); }
         }
+        // midtemp 추가
+        private double midTemp = 15.0;
+        public double MidTemperature
+        {
+            get { return midTemp; }
+            set
+            {
+                midTemp = value;
+                temperatureStep = (temperatureTube.ActualHeight - (bulb.ActualHeight / 2)) / (maxTemp - minTemp);
+                NotifyPropertyChanged(nameof(TemperatureHeight));
+                NotifyPropertyChanged(nameof(MidTemperatureStr));
 
-        private double maxTemp = 50.0;
+            }
+        }
+        private double maxTemp = 40.0;
         public double MaxTemperature
         {
             get { return maxTemp; }
@@ -76,6 +78,22 @@ namespace Thermometer
             }
         }
 
+        public static readonly DependencyProperty TemperatureProperty =
+            DependencyProperty.Register(
+                "Temperature", 
+                typeof(double), 
+                typeof(UserControl1), 
+                new PropertyMetadata(37.0)
+            );
+
+        public string MinTemperatureStr
+        {
+            get { return $"{(int)minTemp}°" + (isCelsius ? "C" : "F"); }
+        }
+        public string MidTemperatureStr
+        {
+            get { return $"{(int)midTemp}°" + (isCelsius ? "C" : "F"); }
+        }
         public string MaxTemperatureStr
         {
             get { return $"{(int)maxTemp}°" + (isCelsius ? "C" : "F"); }
